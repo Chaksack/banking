25e7c325ce46e7fbb193a87933513f82e15058a6
Chakdahah Sackey <andychakdahahsackey@gmail.com>
2022-02-22 23:12:39 +0000

    adding gorilla mux

--- app/app.go
+++ app/app.go
@@ -1,16 +1,29 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
+	// mux := http.NewServeMux()
+	router := mux.NewRouter()
+
 	//define routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customer", getAllCustomers)
+	router.HandleFunc("/greet", greet)
+	router.HandleFunc("/customer", getAllCustomers)
+	router.HandleFunc("/customer/{customer_id:[0-9]+}", getCustomer)
+
 	//starting server
 	log.Fatal(
-		http.ListenAndServe("localhost:8000", nil),
+		http.ListenAndServe("localhost:8000", router),
 	)
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
 }
